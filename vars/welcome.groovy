ca2be70ea21a020197b3449249937b817a10e9c3
sunkaravineeth <63944740+sunkaravineeth@users.noreply.github.com>
2021-09-11 12:33:24 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,31 +1,16 @@
 def call(int buildNumber) {
-  if (buildNumber % 2 == 0) {
-    pipeline {
-      agent any
-      stages {
-        stage('Even Stage') {
-          steps {
-            echo "The build number is even main branch"
-          }
+  pipeline {
+    agent {
+        docker { 
+          image 'node:14-alpine' 
         }
-      }
     }
-  } else {
-    pipeline {
-      agent any
-      stages {
-        stage('Odd Stage') {
-          steps {
-            echo "The build number is odd main branch"
-          }
+    stages {
+        stage('Test') {
+            steps {
+                sh 'node --version'
+            }
         }
-      }
     }
-  }
-}
-
-
-def getNumber(int buildNumber) {
-  echo "The build number is ${buildNumber}"
 }
   
