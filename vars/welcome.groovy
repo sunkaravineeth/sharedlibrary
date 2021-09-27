f4b824da9020a262cd93d078a245572aee7ef0f2
sunkaravineeth <63944740+sunkaravineeth@users.noreply.github.com>
2021-09-27 15:17:33 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,5 +1,5 @@
 
-def call() {
+def call(Map config) {
   if (currentBuild.getBuildCauses().toString().contains('BranchIndexingCause')) {
   print "INFO: Build skipped due to trigger being Branch Indexing"
   currentBuild.result = 'ABORTED' // optves a better hint to the user tha's been skipped, rather than the default which shows it's successful
